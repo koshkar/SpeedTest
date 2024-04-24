c10db52e3e52efce9ceb37ab7ff06bd4df51f42c
koshkar <qwerty376l@mail.ru>
2024-04-24 13:23:06 +0300

    setup main file with enviroment

--- SpeedTestIos/Sources/SpeedTestIosApp.swift
+++ SpeedTestIos/Sources/SpeedTestIosApp.swift
@@ -1,10 +1,14 @@
-import SwiftUI
+import SwiftUI  // Фреймворк для построения пользовательского интерфейса SwiftUI
 
-@main
-struct SpeedTestIosApp: App {
-    var body: some Scene {
-        WindowGroup {
-            MainView()
+@main  // Указывает на точку входа в приложение
+struct SpeedTestIosApp: App {  // Основная структура приложения
+    let coreDataManager = CoreDataManager.shared  // Менеджер Core Data
+    @Environment(\.managedObjectContext) var managedObjectContext  // Контекст Core Data из окружения
+
+    var body: some Scene {  // Основное представление приложения
+        WindowGroup {  // Группа окон, представляющая приложение
+            MainView()  // Основное представление приложения
+                .environment(\.managedObjectContext, coreDataManager.container.viewContext)  // Передаем контекст Core Data
         }
     }
 }
