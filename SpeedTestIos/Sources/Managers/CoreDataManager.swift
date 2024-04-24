ec465039e1edf8acc6def8e9794d691eb1ca51fe
koshkar <qwerty376l@mail.ru>
2024-04-24 13:22:26 +0300

    Add manager, that helps work with Core Data

--- SpeedTestIos/Sources/Managers/CoreDataManager.swift
+++ SpeedTestIos/Sources/Managers/CoreDataManager.swift
@@ -1,8 +1,30 @@
-//
-//  CoreDataManager.swift
-//  SpeedTestIos
-//
-//  Created by Кирилл Кошкарёв on 23.04.2024.
-//
+import CoreData
+import SwiftUI
 
-import Foundation
+// Создание и управление Core Data Stack
+class CoreDataManager {
+    static let shared = CoreDataManager()  // Singleton
+
+    let container: NSPersistentContainer  // Core Data контейнер
+
+    init() {
+        container = NSPersistentContainer(name: "CoreDataModel")  // Название модели данных
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Ошибка при загрузке Core Data: \(error)")
+            }
+        }
+    }
+
+    func saveContext() {  // Сохранение изменений в Core Data
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Ошибка при сохранении Core Data: \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
